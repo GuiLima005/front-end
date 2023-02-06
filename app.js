31caa5f75e46948e86cee5499c13c3ff60236402
GUILHERME DE OLIVEIRA LIMA <22282086@CFP127ALUNO.LOCAL>
2023-02-06 11:23:47 -0300

    Adicionando

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const calcular = document.getElementById('calcular')
+
+function status(){
+    const nota1 = Number(document.getElementById('nota1').value)
+    const nota2 = Number(document.getElementById('nota2').value)
+    const situacao = document.getElementById('situacao')
+    const formulario = document.getElementById('form')
+
+    
+
+    if (situacao.value = (nota1 + nota2) / 2 >=5) {
+        situacao.value = 'APROVADO'
+        formulario.classList.add('aprovado') 
+        formulario.classList.remove('reprovado')
+        
+    } else {
+        situacao.value = 'REPROVADO'
+        formulario.classList.add('reprovado')
+        formulario.classList.remove('aprovado')
+    }
+}
+calcular.addEventListener('click', status)
+
+/*
+const calcular = document.getElementById('calcular')
+
+function somar(){
+    const numero1 = Number(document.getElementById('numero1').value)
+    const numero2 = Number(document.getElementById('numero2').value)
+    const resultado = document.getElementById('resultado')
+    const formulario = document.getElementById('form')
+
+    resultado.value = numero1 + numero2
+    formulario.classList.add('vermelho')
+}
+
+calcular.addEventListener('click', somar)
+*/
